3dc642691eaf9b9fa82246f97a37f456b7828166
kaeden brooks <k8776594@gmail.com>
2025-12-08 15:57:39 -0700

    score board

--- src/game-objects/score-board.ts
+++ src/game-objects/score-board.ts
@@ -1,28 +1,65 @@
+import type { ScoreEvent } from "../game-events";
+import { getCookie, setCookie } from "typescript-cookie";
+
+const MAX_SCORE_COOKIE : string = "max-score";
+
 export class ScoreBoard {
-  private currentScore: number = 123456;
+	private currentScore: number = 0;
+	private maxScore: number = 0;
 
-  constructor(
-    private readonly ctx: CanvasRenderingContext2D,
-    private x: number,
-    private y: number,
-    private w: number,
-    private h: number
-  ) {}
+	constructor(
+		private readonly ctx: CanvasRenderingContext2D,
+		private x: number,
+		private y: number,
+		private w: number,
+		private h: number
+	) {
+    this.maxScore = Math.floor(
+      (getCookie(MAX_SCORE_COOKIE) as number | undefined) ?? 0
+    );
+    this.wireUpEvents();
+  }
 
-  public draw(): void {
-    const { ctx, x, y, w, h, currentScore } = this;
+	public draw(): void {
+		const { ctx, x, y, w, h, currentScore, maxScore } = this;
 
-    ctx.save();
+		// save the current state of our ctx
+		ctx.save();
 
-    let currentScoreX = x + w / 2;
-    let currentScoreY = y + h / 2;
+		let currentScoreX = x + w / 2;
+		let currentScoreY = y + h / 2 + 15;
 
-    ctx.font = "20px fantasy";
-    ctx.textAlign = "center";
-    ctx.fillStyle = "white";
+		ctx.font = "30px Science Gothic";
+		ctx.textAlign = "center";
+		ctx.fillStyle = "white";
 
-    ctx.fillText(currentScore.toString(), currentScoreX, currentScoreY);
+		ctx.fillText(currentScore.toLocaleString(), currentScoreX, currentScoreY);
 
-    ctx.restore();
+		ctx.font = "20px Science Gothic";
+		ctx.textAlign = "left";
+		ctx.fillStyle = "gold";
+
+		ctx.fillText(maxScore.toLocaleString(), x + 20, y + 25);
+
+		ctx.restore();
+	}
+
+  private wireUpEvents() : void {
+    this.onScore = this.onScore.bind(this);
+    window.addEventListener("bb-score", this.onScore);
+
+    this.onGameOver = this.onGameOver.bind(this);
+    window.addEventListener("bb-game-over", this.onGameOver);
+  }
+
+  private onScore(e: ScoreEvent) {
+    console.log("score board listener", e.score);
+    this.currentScore += e.score.total();
+  }
+
+  private onGameOver() :  void {
+    if(this.currentScore > this.maxScore) {
+      setCookie(MAX_SCORE_COOKIE, this.currentScore);
+    }
   }
 }
